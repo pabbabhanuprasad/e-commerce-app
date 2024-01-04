5d686add98fede24c7a3111a0b357aab1d499d65
pabbabhanuprasad <pabbabhanuprasad1@gmail.com>
2024-01-04 15:56:45 +0530

    e-commerce

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
+import Cart from './Pages/Cart';
+import NavBar from './Components/NavBar';
+import {Provider} from 'react-redux';
+import store from './store/store';
+function App() {
+
+  return (
+    <div className="App">
+      <Provider store={store}>
+      <BrowserRouter>
+      <NavBar/>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/cart" element={<Cart/>}></Route>
+        </Routes>
+      </BrowserRouter>
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
